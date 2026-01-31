cab950e76660e7f8a0cbdddcf083084e27cb1b28
Carlos Rozo <alejandrorozokk@gmail.com>
2026-01-31 11:18:25 -0400

    ss

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -12249,7 +12249,39 @@ j_odyssey_j_professions_student = {
  },
  
  },
- },
+  Other = {
+    e_negative = {
+      name = "Negative",
+      text = {
+        "{C:attention}+#1#{} Joker slot"
+      }
+    },
+    e_negative_consumable = {
+      name = "Negative",
+      text = {
+        "{C:attention}+#1#{} Consumable slot"
+      }
+    },
+    e_foil = {
+      name = "Foil",
+      text = {
+        "{C:chips}+#1#{} Chips"
+      }
+    },
+    e_holo = {
+      name = "Holographic",
+      text = {
+        "{C:mult}+#1#{} Mult"
+      }
+    },
+    e_polychrome = {
+      name = "Polychrome",
+      text = {
+        "{X:mult,C:white} X#1# {} Mult"
+      }
+    }
+  },
+  },
  misc = {
  poker_hands = {
  
